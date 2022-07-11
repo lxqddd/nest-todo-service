e22537aa3c66cb6006dafb146d828c0c52f7895f
liuxq <xqliu@relaper.com>
2022-07-11 14:00:43 +0800

    feat: create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { CatsModule } from './cats/cats.module'
+import { UserModule } from './user/user.module'
 @Module({
-  imports: [CatsModule],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/cats/cats.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common"
-
-@Injectable()
-export class CatsService {
-  getCatInfo(query) {
-    console.log(query)
-    return query
-  }
-}
